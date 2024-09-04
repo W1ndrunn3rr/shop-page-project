f3a63c6d3534d5c18e4541c914c4e4e65664d10c
Jakub Gilewicz <123320214+W1ndrunn3rr@users.noreply.github.com>
2024-09-04 23:59:37 +0200

    Update BackgroundImage.tsx

--- src/components/home/BackgroundImage.tsx
+++ src/components/home/BackgroundImage.tsx
@@ -6,7 +6,7 @@ const BackgroundImage = (): React.ReactElement => {
     <div className="relative m-10 overflow-hidden group">
       <img
         className="w-full h-auto object-cover transition-transform duration-500 transform group-hover:scale-110"
-        src="./src/assets/background.jpg"
+        src="https://images.pexels.com/photos/298863/pexels-photo-298863.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
         alt="Stylish clothing background"
       />
       <div className="absolute inset-0 bg-black bg-opacity-40 flex flex-col items-center justify-center">
